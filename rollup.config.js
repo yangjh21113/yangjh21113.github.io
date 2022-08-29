d59f90e30783bf3a81897b6796d2cd39946198e7
yangjihong <jihongyang@shannonai.com>
2022-08-29 20:39:55 +0800

    feat: 使用rollup打包项目

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import serve from 'rollup-plugin-serve';
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife',
+    name: 'bundleName',
+    globals: {
+      lodash: '_',
+      jquery: '$',
+    },
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    commonjs(),
+    resolve(),
+    //typescript(),
+    terser(),
+    postcss(),
+    serve({
+      open: true,
+      port: 3001,
+      contentBase: './dist',
+    }),
+  ],
+  external: ['lodash', 'jquery'],
+};
