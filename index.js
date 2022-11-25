b73b9e4af3dc0bbcfc339482f5da69733debd806
yangjihong <jihongyang@shannonai.com>
2022-11-25 21:16:14 +0800

    feat: new function

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-{
-    let a = 1
-    console.log(a)
-}
-{
-    let a = 1
-    console.log(a)
-}
+const res = JSON.stringify(new Function('return' + `{ "id": 10393, name: 'zhangxinxu', 'date': '2022-04-30' }`)())
+console.log(res)
+
