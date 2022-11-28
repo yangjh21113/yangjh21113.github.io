043547358544d6214acc0df3437d5d63693a6159
yangjihong <jihongyang@shannonai.com>
2022-11-28 23:07:33 +0800

    feat: requestAnimationFrame模拟定时器

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-function checkOrderStatus() {
-    console.log(1);
-}
-
-function checking() {
-    setTimeout(() => {
-        // 假设 checkOrderStatus是一个异步函数
-        checkOrderStatus().then((res) => {
-            // 满足条件停止递归
-            if (res) {
-                //
-            } else {
-                checking()
-            }
-        })
-    }, 3000);
-}
-
-//setInterval(() => {
-//    checkOrderStatus()
-//}, 1000)
-
+function setIntervalPrecision(callback, delay) {
+    // 生成并记录定时器ID
+    let obj = window.interValPrecisionObj || (window.interValPrecisionObj = { num: 0 })
+    obj.num++
+    obj['n' + obj.num] = true
+    var intervalId = obj.num
+    // 开始时间
+    var startTime = +new Date()
+    // 已执行次数
+    var count = 0
+    // 延迟时间
+    delay = delay || 0
+  
+  
+    ;(function loop() {
+      // 定时器被清除，则终止
+      if (!obj['n' + intervalId]) return
+  
+      // 满足条件执行回调
+      if (+new Date() > startTime + delay * (count + 1)) {
+        count++
+        callback(count)
+      }
+  
+      requestAnimationFrame(loop)
+    })()
+  
+    return intervalId
+  }
+  
