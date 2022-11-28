55117d614bb3d393653ea38720f519001c2e872e
yangjihong <jihongyang@shannonai.com>
2022-11-28 21:54:39 +0800

    feat: 定时器

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const res = JSON.stringify(new Function('return' + `{ "id": 10393, name: 'zhangxinxu', 'date': '2022-04-30' }`)())
-console.log(res)
+function checkOrderStatus() {
+    console.log(1);
+}
+
+function checking() {
+    setTimeout(() => {
+        // 假设 checkOrderStatus是一个异步函数
+        checkOrderStatus().then((res) => {
+            // 满足条件停止递归
+            if (res) {
+                //
+            } else {
+                checking()
+            }
+        })
+    }, 3000);
+}
+
+//setInterval(() => {
+//    checkOrderStatus()
+//}, 1000)
 
